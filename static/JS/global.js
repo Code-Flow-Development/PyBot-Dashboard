51b73a2f1f044316dff041b846ff9627d1a8a84a
Puyodead1 <23562356riley@gmail.com>
2019-12-02 19:09:18 -0500

    Update JS file to temp allow banning and unbanning users again

--- static/JS/global.js
+++ static/JS/global.js
@@ -112,30 +112,31 @@ $(document).on('click', '.unban-server-btn', function (e) {
     e.preventDefault();
 });
 //
-// $(document).on('click', '.ban-user-btn', function (e) {
-//     const user_id = $(this).attr('data-userid');
-//     $.ajax("/api/v1/admin/banUser", {
-//         data: JSON.stringify({user_id}),
-//         contentType: "application/json",
-//         type: "POST",
-//         success: function (data, textStatus, jQxhr) {
-//             location.reload();
-//         },
-//     });
-//     e.preventDefault();
-// });
-// $(document).on('click', '.unban-user-btn', function (e) {
-//     const user_id = $(this).attr('data-userid');
-//     $.ajax("/api/v1/admin/unbanUser", {
-//         data: JSON.stringify({user_id}),
-//         contentType: "application/json",
-//         type: "POST",
-//         success: function (data, textStatus, jQxhr) {
-//             location.reload();
-//         },
-//     });
-//     e.preventDefault();
-// });
+$(document).on('click', '.ban-user-btn', function (e) {
+    const user_id = $(this).attr('data-userid');
+    $.ajax("/api/v1/admin/banUser", {
+        data: JSON.stringify({user_id}),
+        contentType: "application/json",
+        type: "POST",
+        success: function (data, textStatus, jQxhr) {
+            location.reload();
+        },
+    });
+    e.preventDefault();
+});
+//
+$(document).on('click', '.unban-user-btn', function (e) {
+    const user_id = $(this).attr('data-userid');
+    $.ajax("/api/v1/admin/unbanUser", {
+        data: JSON.stringify({user_id}),
+        contentType: "application/json",
+        type: "POST",
+        success: function (data, textStatus, jQxhr) {
+            location.reload();
+        },
+    });
+    e.preventDefault();
+});
 
 $(document).ready(function () {
     $('.theme-select').on('click', function (e) {
